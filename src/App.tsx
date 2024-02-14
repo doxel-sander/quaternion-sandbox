b7dafd2806e4a6e6e793250c6d3915b198267118
Sander <sander@doxel.ai>
2024-02-14 09:44:34 -0500

    snapshot

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,29 @@ export const App = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
-  const [w, setw] = useState(1);
+  const [w, setW] = useState(1);
 
-  const quaternion = new THREE.Quaternion(x, y, z, w);
-
+  const quaternion = new THREE.Quaternion(x, y, z, w).normalize();
 
   // RENDER
   // ==================
   return <>
     <div className="controls">
+      <h2>Controls</h2>
+      <label>X: {x} (actual: {quaternion.x})<br />
+        <input type="range" min="-1" max="1" step="0.1" value={x} onChange={evt => setX(+evt.target.value)} />
+      </label>
+      <label>Y: {y} (actual: {quaternion.y})<br />
+        <input type="range" min="-1" max="1" step="0.1" value={y} onChange={evt => setY(+evt.target.value)} />
+      </label>
+
+      <label>Z: {z} (actual: {quaternion.z})<br />
+        <input type="range" min="-1" max="1" step="0.1" value={z} onChange={evt => setZ(+evt.target.value)} />
+      </label>
+
+      <label>W: {w} (actual: {quaternion.w})<br />
+        <input type="range" min="-1" max="1" step="0.1" value={w} onChange={evt => setW(+evt.target.value)} />
+      </label>
 
     </div>
 
