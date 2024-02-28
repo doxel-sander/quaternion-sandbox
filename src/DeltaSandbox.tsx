28f3ee8ae9c3eab09ad7ecd497e78f7633f16885
Sander <sander@doxel.ai>
2024-02-28 14:37:52 -0500

    cleanup

--- src/DeltaSandbox.tsx
+++ src/DeltaSandbox.tsx
@@ -26,54 +26,9 @@ export const DeltaSandbox = ({
     // Deltas to maintain
     const [deltas, setDeltas] = useState<[THREE.Vector3, THREE.Vector3][]>([])
     const onMeshClick = (evt: ThreeEvent<MouseEvent>) => {
-        // const a = 
-
-        // const c1 = 
-
-
         // https://www.movable-type.co.uk/scripts/latlong-vectors.html#bearing
         const from = scratchV3.clone().set(...cameraPosition).normalize();
         const to = evt.point.clone().normalize();
-
-        // get angular distance
-
-
-
-
-
-        // // rotation plane
-        // const axis = from.clone().cross(to)
-        // const angle = from.angleTo(to);
-        // const q = scratchQ.clone().setFromUnitVectors(from, to).normalize();
-        // const e = new THREE.Euler().setFromQuaternion(q);
-        // const dot = from.clone().dot(to.clone());
-        // if (dot > 0.999999) {
-        //     return scratchQ.clone().identity();
-        // } else if (dot < -0.999999) {
-        //     return scratchQ.clone().set(0, 1, 0, 0);
-        // }
-
-        // const cross = from.clone().cross(to.clone());
-        // const w = Math.sqrt(Math.pow(from.length(), 2) * Math.pow(to.length(), 2)) + dot;
-        // const output = scratchQ.clone().set(...cross.toArray(), w).normalize();
-        // const s = new THREE.Spherical().setFromVector3(evt.point)
-        // console.log(evt.point, s)
-        // const delta = scratchV3.clone().set(
-        //     evt.point.x - cameraPosition[0],
-        //     evt.point.y - cameraPosition[1],
-        //     evt.point.z - cameraPosition[2]
-        // );
-        // console.log(delta)
-        // const from = scratchQ.clone().setFromUnitVectors(
-        //     scratchV3.clone().set(...cameraPosition).normalize(),
-        //     scratchV3.clone().set(0, 0, 1)
-        // );
-        // const to = scratchQ.clone().setFromUnitVectors(
-        //     // scratchV3.clone().set(0, 0, 1),
-        //     scratchV3.clone().set(...cameraPosition).normalize(),
-        //     evt.point.clone().normalize()
-        // ).normalize();
-
         setDeltas([...deltas, [from, to]]);
     }
 
